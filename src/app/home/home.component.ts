fe66651ca4646e93c8efed9b12e9548d9148238b
= <=>
2024-07-16 12:27:45 +0000

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+
+  text: string = '---';
+
+  ngOnInit(): void {
+    
+  }
+
+  update(value: any){
+    console.log(value);
+    this.text = value;
+  }
+}
